83ed64bb3e9153ca390c27cefdcdade657711b72
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2025-12-07 00:30:17 +0200

    Add MovieModal component with portal, ESC handling and backdrop close

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,44 +1,64 @@
-import { useState } from 'react';
+// src/components/App/App.tsx
+import { useEffect, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
+
 import css from './App.module.css';
 
 import SearchBar from '../SearchBar/SearchBar';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
 import { searchMovies } from '../../services/api';
 import type { Movie } from '../../types/movie';
 
 export default function App() {
+  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const handleSearch = async (query: string) => {
-    setIsLoading(true);
-    setError(false);
-    setMovies([]); 
+  const handleSearch = (newQuery: string) => {
+    if (newQuery === query) return;
 
-    try {
-      const results = await searchMovies(query);
-
-      if (results.length === 0) {
-        toast.error('No movies found for your request.');
-        return;
-      }
-
-      setMovies(results);
-    } catch {
-      setError(true);
-    } finally {
-      setIsLoading(false);
-    }
+    setQuery(newQuery);
+    setMovies([]);       
+    setSelectedMovie(null);
   };
 
- const handleSelectMovie = (movie: Movie) => {
-  console.log(movie);
-};
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
 
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const results = await searchMovies(query);
+
+        if (results.length === 0) {
+          setMovies([]);
+          toast.error('No movies found for your request.');
+          return;
+        }
+
+        setMovies(results);
+      } catch {
+        setError('true');
+        toast.error('There was an error, please try again...');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [query]);
 
   return (
     <div className={css.app}>
@@ -52,10 +72,18 @@ export default function App() {
         <MovieGrid movies={movies} onSelect={handleSelectMovie} />
       )}
 
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+
       <Toaster position="top-right" />
     </div>
   );
 }
+
 
 
 
