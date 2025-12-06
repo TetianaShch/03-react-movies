b0e516e3980040bbd19c0971005537ded9190c0f
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2025-12-06 20:25:43 +0200

    Add SearchBar component with form handling and toast notifications

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,22 @@
+import { Toaster } from 'react-hot-toast';
+import SearchBar from '../SearchBar/SearchBar';
 import css from './App.module.css';
 
 export default function App() {
-  return <div className={css.app}>Movie search app</div>;
+  const handleSearch = (query: string) => {
+   void query; // unused
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+
+      {/*  MovieGrid, Loader, ErrorMessage, MovieModal */}
+      <Toaster position="top-right" />
+    </div>
+  );
 }
+
 
 
 
